7f62b89aef0ea856bc51e96b4d02fb4c17f605a4
DS2016-S2A0B2R0X <16843417+DS2016-S2A0B2R0X@users.noreply.github.com>
2022-01-04 23:10:38 +0100

    20220104_SBO_U_PUB Update Install

--- Install_Sentiment.R
+++ Install_Sentiment.R
@@ -3,6 +3,7 @@
 
 install.packages("devtools")
 require(devtools)
+# site down does not exist anymore
 install_url("http://www.omegahat.org/Rstem/Rstem_0.4-1.tar.gz")
 install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.1.tar.gz")
 install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.2.tar.gz")
