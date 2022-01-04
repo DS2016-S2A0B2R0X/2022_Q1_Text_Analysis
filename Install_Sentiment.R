0175e7e4be61d2d355e90bb83e80848c4f46ee31
DS2016-S2A0B2R0X <16843417+DS2016-S2A0B2R0X@users.noreply.github.com>
2022-01-04 23:00:26 +0100

    20220104_SBO_C_PUB Initial commit

---
+++ Install_Sentiment.R
@@ -0,0 +1,9 @@
+#https://www.r-bloggers.com/2016/01/intro-to-text-analysis-with-r/
+
+
+install.packages("devtools")
+require(devtools)
+install_url("http://www.omegahat.org/Rstem/Rstem_0.4-1.tar.gz")
+install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.1.tar.gz")
+install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.2.tar.gz")
+
